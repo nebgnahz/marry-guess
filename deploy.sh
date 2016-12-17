170d681a0a7e311c75cb834bb50018b09ab402d5
Ben Zhang <benzh@cs.berkeley.edu>
2016-12-17 14:17:32 -0800

    feat: add url to package

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+npm run build
+
+git checkout -B gh-pages
+git add -f build
+git commit -am "Rebuild website"
+git filter-branch -f --prune-empty --subdirectory-filter build
+git push -f origin gh-pages
+git checkout -
