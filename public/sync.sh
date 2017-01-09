94918316e160b20f901ed62f0912a6b100282f47
Ben Zhang <benzh@cs.berkeley.edu>
2017-01-08 16:48:58 -0800

    fix: commit result pages

---
+++ public/sync.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/bash
+
+declare -A answers=(
+    ["volcano"]="At a Volcano" ["castle"]="At a Castle"
+               ["plane"]="On a Plane" ["rollercoaster"]="On a Roller Coaster"
+               ["balloon"]="In a Hot Balloon"
+               ["underwater"]="Under the Water"
+               ["glacier"]="On the Glacier"
+               ["boat"]="On a Boat"
+               ["treehouse"]="In a Tree House"
+               ["museum"]="In a Museum"
+)
+
+for ans in "${!answers[@]}";
+do
+    echo "$ans - ${answers[$ans]}";
+    cp template.html $ans.html
+    sed -i '' -e "s/xxxxx/$ans/g" $ans.html
+    sed -i '' -e "s/yyyyyyyyy/${answers[$ans]}/g" $ans.html
+done
